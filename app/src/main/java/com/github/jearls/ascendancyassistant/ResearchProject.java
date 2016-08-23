d1eae24502d56d3b846b56602bc365fca6d860c7
Johnson Earls <darkfoxprime@users.noreply.github.com>
2016-08-23 09:45:08 -0700

    Add research project classes

---
+++ app/src/main/java/com/github/jearls/ascendancyassistant/ResearchProject.java
@@ -0,0 +1,104 @@
+package com.github.jearls.ascendancyassistant;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * A research project consists of a name, a set of technologies, a set
+ * of dependencies on other reesearch projects, flags indicating if the
+ * project is complete, is a goal, or is in the research path towards a
+ * goal, and goal and research path numbers, if the associated flags are
+ * true.
+ *
+ * @author Johnson Earls
+ */
+public class ResearchProject {
+    String name;
+    Set<String> technologies;
+    Set<ResearchProject> dependencies;
+    boolean complete;
+    boolean goal;
+    boolean inPath;
+    int goalNumber;
+    int pathNumber;
+
+    public ResearchProject(String name) {
+        this.name = name;
+        this.technologies = new HashSet<>();
+        this.dependencies = new HashSet<>();
+        this.complete = false;
+        this.goal = false;
+        this.inPath = false;
+        this.goalNumber = 0;
+        this.pathNumber = 0;
+    }
+
+    public boolean isInPath() {
+        return inPath;
+    }
+
+    public void setInPath(boolean inPath) {
+        this.inPath = inPath;
+    }
+
+    public int getPathNumber() {
+        return pathNumber;
+    }
+
+    public void setPathNumber(int pathNumber) {
+        this.pathNumber = pathNumber;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public boolean isGoal() {
+        return goal;
+    }
+
+    public void setGoal(boolean goal) {
+        this.goal = goal;
+    }
+
+    public int getGoalNumber() {
+        return goalNumber;
+    }
+
+    public void setGoalNumber(int goalNumber) {
+        this.goalNumber = goalNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Iterator<String> getTechnologies() {
+        return technologies.iterator();
+    }
+
+    public void addTechnology(String technology) {
+        technologies.add(technology);
+    }
+
+    public void removeTechnology(String technology) {
+        technologies.remove(technology);
+    }
+
+    public Iterator<ResearchProject> getDependencies() {
+        return dependencies.iterator();
+    }
+
+    public void addDependency(ResearchProject dependency) {
+        dependencies.add(dependency);
+    }
+
+    public void removeDependency(ResearchProject dependency) {
+        dependencies.remove(dependency);
+    }
+}
---
+++ app/src/main/java/com/github/jearls/ascendancyassistant/ResearchTree.java
@@ -0,0 +1,170 @@
+package com.github.jearls.ascendancyassistant;
+
+import android.content.res.XmlResourceParser;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * The research "tree" is really just a map of project names to
+ * projects.
+ *
+ * @author Johnson Earls
+ */
+public class ResearchTree {
+    Map<String, ResearchProject> researchProjects = new HashMap<>();
+
+    public ResearchTree() {
+    }
+
+    public static ResearchTree loadXmlResearchTree(XmlResourceParser parser) throws IOException {
+        final int STATE_START = 0;
+        final int STATE_SUCCESS = 1;
+        final int STATE_ERROR = 2;
+        final int STATE_DOCUMENT = 3;
+        final int STATE_RESEARCH_TREE = 4;
+        final int STATE_RESEARCH_PROJECT = 5;
+        final int STATE_REQUIREMENTS = 6;
+        final int STATE_REQUIRES = 7;
+        final int STATE_TECHNOLOGIES = 8;
+        final int STATE_TECHNOLOGY = 9;
+
+        final int TAG_START_DOCUMENT = 1;
+        final int TAG_END_DOCUMENT = 2;
+        final int TAG_START_RESEARCH_TREE = "researchtree".hashCode();
+        final int TAG_END_RESEARCH_TREE = TAG_START_RESEARCH_TREE + 1;
+        final int TAG_START_RESEARCH_PROJECT = "researchproject".hashCode();
+        final int TAG_END_RESEARCH_PROJECT = TAG_START_RESEARCH_PROJECT + 1;
+        final int TAG_START_REQUIREMENTS = "requirements".hashCode();
+        final int TAG_END_REQUIREMENTS = TAG_START_REQUIREMENTS + 1;
+        final int TAG_START_REQUIRES = "requires".hashCode();
+        final int TAG_END_REQUIRES = TAG_START_REQUIRES + 1;
+        final int TAG_START_TECHNOLOGIES = "technologies".hashCode();
+        final int TAG_END_TECHNOLOGIES = TAG_START_TECHNOLOGIES + 1;
+        final int TAG_START_TECHNOLOGY = "technology".hashCode();
+        final int TAG_END_TECHNOLOGY = TAG_START_TECHNOLOGY + 1;
+
+        int nextState = STATE_START, state = STATE_START;
+        ResearchProject project = null;
+        ResearchTree tree = new ResearchTree();
+
+        while (nextState != STATE_ERROR && nextState != STATE_SUCCESS) {
+            state = nextState;
+            nextState = STATE_ERROR;
+            try {
+                int tag = parser.nextTag();
+                switch (tag) {
+                    case XmlResourceParser.START_DOCUMENT:
+                        tag = TAG_START_DOCUMENT;
+                        break;
+                    case XmlResourceParser.END_DOCUMENT:
+                        tag = TAG_END_DOCUMENT;
+                        break;
+                    case XmlResourceParser.START_TAG:
+                        tag = parser.getName().toLowerCase().hashCode();
+                        break;
+                    case XmlResourceParser.END_TAG:
+                        tag = parser.getName().toLowerCase().hashCode() + 1;
+                        break;
+                    default:
+                        tag = 0;
+                }
+                String name = parser.getAttributeValue(null, "name");
+                switch (state) {
+                    case STATE_START:
+                        if (tag == TAG_START_DOCUMENT) {
+                            nextState = STATE_DOCUMENT;
+                        }
+                        break;
+                    case STATE_DOCUMENT:
+                        if (tag == TAG_END_DOCUMENT) {
+                            nextState = STATE_SUCCESS;
+                        } else if (tag == TAG_START_RESEARCH_TREE) {
+                            nextState = STATE_RESEARCH_TREE;
+                        }
+                        break;
+                    case STATE_RESEARCH_TREE:
+                        if (tag == TAG_END_RESEARCH_TREE) {
+                            nextState = STATE_DOCUMENT;
+                        } else if (tag == TAG_START_RESEARCH_PROJECT) {
+                            nextState = STATE_RESEARCH_PROJECT;
+                            project = tree.researchProjects.get(name);
+                            if (project == null) {
+                                project = new ResearchProject(name);
+                                tree.researchProjects.put(name, project);
+                            }
+                        }
+                        break;
+                    case STATE_RESEARCH_PROJECT:
+                        if (tag == TAG_END_RESEARCH_PROJECT) {
+                            nextState = STATE_RESEARCH_TREE;
+                        } else if (tag == TAG_START_REQUIREMENTS) {
+                            nextState = STATE_REQUIREMENTS;
+                        } else if (tag == TAG_START_TECHNOLOGIES) {
+                            nextState = STATE_TECHNOLOGIES;
+                        }
+                        break;
+                    case STATE_REQUIREMENTS:
+                        if (tag == TAG_END_REQUIREMENTS) {
+                            nextState = STATE_RESEARCH_PROJECT;
+                        } else if (tag == TAG_START_REQUIRES) {
+                            nextState = STATE_REQUIRES;
+                            ResearchProject dependency = tree.researchProjects.get(name);
+                            if (dependency == null) {
+                                dependency = new ResearchProject(name);
+                                tree.researchProjects.put(name, dependency);
+                            }
+                            project.addDependency(dependency);
+                        }
+                        break;
+                    case STATE_REQUIRES:
+                        if (tag == TAG_END_REQUIRES) {
+                            state = STATE_REQUIREMENTS;
+                        }
+                        break;
+                    case STATE_TECHNOLOGIES:
+                        if (tag == TAG_END_TECHNOLOGIES) {
+                            nextState = STATE_RESEARCH_PROJECT;
+                        } else if (tag == TAG_START_TECHNOLOGY) {
+                            nextState = STATE_TECHNOLOGY;
+                            project.addTechnology(name);
+                        }
+                        break;
+                    case STATE_TECHNOLOGY:
+                        if (tag == TAG_END_TECHNOLOGY) {
+                            state = STATE_TECHNOLOGIES;
+                        }
+                        break;
+                }
+            } catch (XmlPullParserException | IOException e) {
+                state = STATE_ERROR;
+            }
+        }
+        if (state != STATE_SUCCESS) {
+            throw new IOException("Invalid XML research tree");
+        }
+        return tree;
+    }
+
+    public ResearchProject getResearchProject(String name) {
+        return researchProjects.get(name);
+    }
+
+    public void addResearchProject(ResearchProject project) {
+        researchProjects.put(project.getName(), project);
+    }
+
+    public void removeResearchProject(ResearchProject project) {
+        researchProjects.remove(project.getName());
+    }
+
+    public Collection<ResearchProject> getResearchProjects() {
+        return researchProjects.values();
+    }
+}
